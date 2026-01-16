79f6f51285e09b83b178f124032358ab31d41571
Bobbe <maxschlecht@web.de>
2026-01-16 17:31:11 +0100

    Refactor hardware config, remove swap device

--- host/hardware-configuration.nix
+++ host/hardware-configuration.nix
@@ -1,22 +1,21 @@
 {
     config,
     lib,
-    pkgs,
     modulesPath,
     ...
 }: {
-    imports = [
-        (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+    imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
-    boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod"];
+    boot.initrd.availableKernelModules = [
+        "ahci"
+        "xhci_pci"
+        "nvme"
+        "usb_storage"
+        "sd_mod"
+    ];
     boot.initrd.kernelModules = [];
     boot.kernelModules = ["kvm-amd"];
     boot.extraModulePackages = [];
-
-    swapDevices = [
-        {device = "/dev/disk/by-uuid/7e30ce44-c5fa-4bc7-a98d-a6026469fb1f";}
-    ];
 
     networking.useDHCP = lib.mkDefault true;
 
